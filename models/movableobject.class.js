e2edec00b1f464b33a3d47a516e6528e64310c03
Analphabet <robert.jordan@gmx.net>
2025-03-26 09:43:38 +0100

    Included js files in html, playAnimation

--- models/movableobject.class.js
+++ models/movableobject.class.js
@@ -8,4 +8,11 @@ class MoveableObject extends DrawableObject {
   moveLeft() {
     this.x -= this.speed;
   }
+  
+   playAnimation(images) {
+        let i = this.currentImage % images.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
   }
