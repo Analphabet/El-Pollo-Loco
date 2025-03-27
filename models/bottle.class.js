3d721cbe1ce0f8859e025f071074e7c32a9b7d02
Analphabet <robert.jordan@gmx.net>
2025-03-27 15:42:21 +0100

    bottles classes

---
+++ models/bottle.class.js
@@ -0,0 +1,18 @@
+
+class Bottles extends MoveableObject {
+
+    width = 60;
+    height = 60;
+    IMAGES_BOTTLE = [
+        'img/salsa/1_salsa_bottle_on_ground.png',
+        'img/salsa/2_salsa_bottle_on_ground.png'
+    ];
+
+    constructor(x, y) {
+        super();
+        this.loadImage(this.IMAGES_BOTTLE[Math.round(Math.random())]);
+        this.x = x + 400;
+        this.y = y;
+        };
+    }
+}
---
+++ models/throwableObject.class.js
@@ -0,0 +1,68 @@
+
+
+class ThrowableObject extends MoveableObject {
+    speedY = 30;
+    speedX = 30;
+    hasCollided;
+    rotationInterval;
+
+    IMAGES_BOTTLE_ROTATION = [
+        'img/salsa/bottle_rotation/1_bottle_rotation.png',
+        'img/salsa/bottle_rotation/2_bottle_rotation.png',
+        'img/salsa/bottle_rotation/3_bottle_rotation.png',
+        'img/salsa/bottle_rotation/4_bottle_rotation.png'
+    ];
+
+    IMAGES_BOTTLE_SPLASH = [
+        'img/salsa/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/salsa/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/salsa/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/salsa/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/salsa/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/salsa/bottle_rotation/bottle_splash/6_bottle_splash.png',
+
+    ];
+
+    constructor(x, y) {
+        super().loadImage('img/salsa/salsa_bottle.png');
+        this.loadImages(this.IMAGES_BOTTLE_ROTATION);
+        this.loadImages(this.IMAGES_BOTTLE_SPLASH);
+        this.x = x;
+        this.y = y;
+        this.height = 80;
+        this.width = 65;
+        this.throwInterval = null;
+        this.throw();
+        this.animate();
+    }
+
+
+    throw() {
+        this.speedY = 30;
+        this.speedX = 30;
+        this.applyGravity();
+        this.throwInterval = setInterval(() => {
+            this.x += 25;
+        }, 60);
+    }
+
+
+   
+    animate() {
+        this.rotationInterval = setInterval(() => {
+            this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
+        }, 30);
+    }
+
+
+  
+    animateBottleSplash() {
+        clearInterval(this.rotationInterval);
+        this.speedX = 0;
+        this.speedY = 0;
+        this.applyGravity(false);
+        clearInterval(this.throwInterval);
+        this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
+    }
+   
+}
